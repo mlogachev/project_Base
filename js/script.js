56e518380a4dcdb3232e65ccba185177619de5da
Mikhail Logachev <miha.loga@gmail.com>
2015-07-22 20:09:10 +0300

    script.js remastered

--- js/script.js
+++ js/script.js
@@ -1,25 +1,13 @@
 var main = function () {
 	'use strict';
 
-	$('.tabs a:nth-child(1)').on('click' , function() {
-		makeTabActive(1);
-		return false;
-	})
-
-	$('.tabs a:nth-child(2)').on('click' , function() {
-		makeTabActive(2);
-		return false;
-	})
-
-	$('.tabs a:nth-child(3)').on('click' , function() {
-		makeTabActive(3);
-		return false;
-	})
-
-	var makeTabActive = function(tabNumber) {
-		$('.tabs span').removeClass('active');
-		$('.tabs a:nth-child(' + tabNumber + ') span').addClass('active');
-		$('main .content').empty();
-	};
+	$('.tabs a span').toArray().forEach(function(element) {
+		$(element).on('click' , function() {
+			$('.tabs a span').removeClass('active');
+			$(element).addClass('active');
+			$('main .content').empty();
+			return false;
+		});
+	});
 };
 $(document).ready(main);
